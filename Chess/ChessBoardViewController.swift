f3170467525608c238d4d44ed4d8cfcef6e24223
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-03-27 16:55:07 -0700

    Yeah move some of the code out of the vc

--- Chess/ChessBoardViewController.swift
+++ Chess/ChessBoardViewController.swift
@@ -44,50 +44,11 @@ extension ChessBoardViewController: UICollectionViewDataSource, UICollectionView
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     guard let square = collectionView.dequeueReusableCell(withReuseIdentifier: ChessBoardSquare.identifier, for: indexPath) as? ChessBoardSquare else { return .init() }
     
-    square.configure(serialOrder: indexPath.row)
-    
-    // TODO: hacky as FUCK maybe put this in the square itself LMAO
     let transposedCoordinates = 63 - (8*(indexPath.row/8)) - (7-(indexPath.row%8))
     let squareInfo = game.board.squares[transposedCoordinates]
-    if let piece = squareInfo.piece {
-      addPieceImage(type: piece.type, color: piece.color, to: square)
-    }
-//    addSquareNumber(transposedCoordinates, to: square)
+    square.configure(serialOrder: indexPath.row, piece: squareInfo.piece)
+    
     return square
-  }
-  
-  private func addPieceImage(type pieceType: Piece.PieceType, color: Color, to square: ChessBoardSquare) {
-    let pieceView: ChessPiece
-    switch pieceType {
-        
-      case .pawn:
-        pieceView = ChessPiecePawn(color: color).autolayoutEnabled
-      case .rook:
-        pieceView = ChessPieceRook(color: color).autolayoutEnabled
-      case .knight:
-        pieceView = ChessPieceKnight(color: color).autolayoutEnabled
-      case .bishop:
-        pieceView = ChessPieceBishop(color: color).autolayoutEnabled
-      case .queen:
-        pieceView = ChessPieceQueen(color: color).autolayoutEnabled
-      case .king:
-        pieceView = ChessPieceKing(color: color).autolayoutEnabled
-    }
-    
-    square.addSubview(pieceView)
-    pieceView.pin(to: square, margins: .init(top: 2, left: 2, bottom: 2, right: 2))
-  }
-  
-  private func addSquareNumber(_ number: Int, to square: ChessBoardSquare) {
-    let label = UILabel().autolayoutEnabled
-    label.text = "\(number)"
-    label.textAlignment = .center
-    label.font = .boldSystemFont(ofSize: UIFont.labelFontSize)
-    label.backgroundColor = .init(white: 1, alpha: 0.75)
-    
-    square.addSubview(label)
-    square.bringSubviewToFront(label)
-    label.pin(to: square)
   }
 }
 
@@ -169,8 +130,12 @@ final class ChessBoardSquare: UICollectionViewCell {
     
   }
   
-  func configure(serialOrder: Int) {
+  func configure(serialOrder: Int, piece: Piece?) {
     backgroundColor = getColor(serialOrder: serialOrder)
+    
+    if let piece = piece {
+      addPieceImage(piece)
+    }
   }
   
   private func getColor(serialOrder: Int) -> UIColor {
@@ -179,5 +144,38 @@ final class ChessBoardSquare: UICollectionViewCell {
     } else {
       return serialOrder.isEven ? .systemMint : .systemBackground
     }
+  }
+  
+  private func addPieceImage(_ piece: Piece) {
+    let pieceView: ChessPiece
+    switch piece.type {
+        
+      case .pawn:
+        pieceView = ChessPiecePawn(color: piece.color).autolayoutEnabled
+      case .rook:
+        pieceView = ChessPieceRook(color: piece.color).autolayoutEnabled
+      case .knight:
+        pieceView = ChessPieceKnight(color: piece.color).autolayoutEnabled
+      case .bishop:
+        pieceView = ChessPieceBishop(color: piece.color).autolayoutEnabled
+      case .queen:
+        pieceView = ChessPieceQueen(color: piece.color).autolayoutEnabled
+      case .king:
+        pieceView = ChessPieceKing(color: piece.color).autolayoutEnabled
+    }
+    
+    addSubview(pieceView)
+    pieceView.pin(to: self, margins: .init(top: 2, left: 2, bottom: 2, right: 2))
+  }
+  
+  private func addSquareNumber(_ number: Int, to square: ChessBoardSquare) {
+    let label = UILabel().autolayoutEnabled
+    label.text = "\(number)"
+    label.textAlignment = .center
+    label.font = .boldSystemFont(ofSize: UIFont.labelFontSize)
+    label.backgroundColor = .init(white: 1, alpha: 0.75)
+    
+    addSubview(label)
+    label.pin(to: self)
   }
 }
